c22d77aa6ab1e2aed6dfe0cd4cdb85da7b23e869
VitorNuness <vitor1nuness@gmail.com>
2024-10-02 14:54:59 -0300

    adding '/login' in web routes

--- tests/Feature/Auth/LoginTest.php
+++ tests/Feature/Auth/LoginTest.php
@@ -1,6 +1,6 @@
 <?php
 
-use function Pest\Laravel\{assertDatabaseCount, assertDatabaseHas, seed};
+use function Pest\Laravel\{assertDatabaseCount, assertDatabaseHas, post, seed};
 
 test('can create an user when seeding the database', function () {
     seed();
@@ -10,4 +10,9 @@ test('can create an user when seeding the database', function () {
         'name'  => 'Simoni Nunes da Silva',
         'email' => 'simoni@email.com',
     ]);
+});
+
+it('should be can access the login route', function () {
+    post('/login')
+        ->assertOk();
 });
