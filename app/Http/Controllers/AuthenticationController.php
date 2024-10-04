90716654834bd3c5efad9427a2e3fe9677acfa1f
VitorNuness <vitor1nuness@gmail.com>
2024-10-04 16:58:53 -0300

    change return of login method, redirecting to dashboard

--- app/Http/Controllers/AuthenticationController.php
+++ app/Http/Controllers/AuthenticationController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\LoginRequest;
+use Illuminate\Http\RedirectResponse;
 
 class AuthenticationController extends Controller
 {
-    public function store(LoginRequest $request): void
+    public function store(LoginRequest $request): RedirectResponse
     {
         $request->authenticate();
         $request->session()->regenerate();
+
+        return to_route('dashboard');
     }
 }
