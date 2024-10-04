837b6e2d2cad0f40f98f77d67daee0413b8a640b
VitorNuness <vitor1nuness@gmail.com>
2024-10-04 10:30:03 -0300

    adding tailwind and primevue themes

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,10 +1,31 @@
 import axios from "axios";
 import { createApp, h } from "vue";
 import { createInertiaApp } from "@inertiajs/vue3";
+import PrimeVue from "primevue/config";
+import Lara from "@primevue/themes/lara";
+import { definePreset } from "@primevue/themes";
 
 window.axios = axios;
 
 window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
+
+const RosePreset = definePreset(Lara, {
+    semantic: {
+        primary: {
+            50: "{rose.50}",
+            100: "{rose.100}",
+            200: "{rose.200}",
+            300: "{rose.300}",
+            400: "{rose.400}",
+            500: "{rose.500}",
+            600: "{rose.600}",
+            700: "{rose.700}",
+            800: "{rose.800}",
+            900: "{rose.900}",
+            950: "{rose.950}",
+        },
+    },
+});
 
 createInertiaApp({
     resolve: (name) => {
@@ -14,6 +35,11 @@ createInertiaApp({
     setup({ el, App, props, plugin }) {
         createApp({ render: () => h(App, props) })
             .use(plugin)
+            .use(PrimeVue, {
+                theme: {
+                    preset: RosePreset,
+                },
+            })
             .mount(el);
     },
 });
